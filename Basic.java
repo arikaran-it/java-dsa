c75ba24a875739a77a4b14f3f6cbe680723bbefb
Ari Karan <arikaran4044@gmail.com>
2025-12-31 21:04:56 +0530

    Java basic

---
+++ Basic.java
@@ -0,0 +1,30 @@
+class Basic {
+    public static void main(String[] args) {
+        String Name="Ari Karan";
+        int Age = 23;
+        System.out.println("My name is: " + Name);
+        System.out.println("Age:"+Age);
+        System.out.println("Add:" + (10+5));
+
+        int num = 10;
+        System.out.println("Num:"+num);
+        float salary = 1000.00f;
+        System.out.println("Salary:"+salary);
+        double salary2 = 1000.00;
+        System.out.println("Salary2:"+salary2);
+        char  salary3 = 'A';
+        System.out.println("Salary3:"+salary3);
+        boolean  salary4 = true;
+        System.out.println("Salary4:"+salary4);
+
+        int one= 5;
+        int two= 10;
+        int temp;
+
+        temp=one;
+        one=two;
+        two=temp;
+        System.out.println("one:"+one);
+        System.out.println("two:"+two);
+    }
+}
