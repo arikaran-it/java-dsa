d74029b28a5265beb7998916c2faf2cde27fdf22
Ari Karan <arikaran4044@gmail.com>
2025-12-31 21:12:35 +0530

    Array Demo

---
+++ ArrayDemo.java
@@ -0,0 +1,12 @@
+class ArrayDemo {
+    public static void main(String[] args) {
+
+        // Declare and initialize array
+        int[] arr = {10, 20, 30, 40, 50};
+
+        // Print array elements using loop
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
