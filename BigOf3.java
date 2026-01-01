a9428329698c17d54c45e1e18455539cbcae9b20
Ari Karan <arikaran4044@gmail.com>
2026-01-01 13:15:07 +0530

    change

--- BigOf3.java
+++ BigOf3.java
@@ -1,15 +1,13 @@
-class BigOf3{
-    public  static void main(String args[]){
+class BigOf3 {
+    public static void main(String args[]) {
         int a = 14;
         int b = 24;
         int c = 34;
-        if(a>=b && a>=c){
+        if (a >= b && a >= c) {
             System.out.println("Greatest: a");
-        }
-        else if (b>=a && b>=c){
+        } else if (b >= a && b >= c) {
             System.out.println("Greatest: b");
-        }
-        else {
+        } else {
             System.out.println("Greatest: c");
         }
     }
